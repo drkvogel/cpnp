251ce71f94fcc78128c26890fad6a23476781696
Chris Bird <chrisjbird@gmail.com>
2015-10-28 15:34:30 +0000

    more

--- merge.py
+++ merge.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
 import os
+import sys
 import csv
+import sqlite3
 #import pandas
 #import pylab
-import sqlite3
-
-
 
 # object for each behavioural episode
+# each behavioural episode will have associated average acceleration data
 class episode:
     def __init__():
         self.start  = 0
@@ -20,24 +20,28 @@ def read_files(cam_csv, acc_csv):
         cam_reader = csv.DictReader(cam_data, delimiter=',', quotechar='"') # unicode_csv_reader()?
         print("importing camera data")
         for row in cam_reader: 
-            print("."), # no newline
+            sys.stdout.write('.')
             #print(row['participant'], row['startTime'])
 
     with open(acc_csv, 'rb') as acc_data:
         acc_reader = csv.DictReader(acc_data, delimiter=',', quotechar='"') # unicode_csv_reader()?
         print("importing accelerometer data")
         for row in acc_reader: 
-            print("."), # no newline
+            sys.stdout.write('.')
             #print(row['timestamp'], row['acceleration'])
-
-# each behavioural episode will have associated average acceleration data
 
 def save_data():
     # merged information should then be stored in a database of your choosing
-    sqlite_file = '/home/kvogel/Projects/cpnp/wearables.sqlite'
+    sqlite_file = './wearables.sqlite'
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
+    print("TODO")
+    exit()
 
+def query():
+    # script should support subsequent querying of the database to describe the average acceleration associated with each behaviour type
+    print("TODO")
+    exit()
 
 if __name__ == '__main__': # running standalone
     ins = True
@@ -47,7 +51,6 @@ if __name__ == '__main__': # running standalone
         print("""
         1. Merge raw data
         2. Query merged data
-        3. something else
             """)
         ins = raw_input("Enter a number or q to quit: ") # http://stackoverflow.com/questions/4915361/whats-the-difference-between-raw-input-and-input-in-python3-x
         if ins == "1":
@@ -57,18 +60,14 @@ if __name__ == '__main__': # running standalone
             read_files(cam_csv, acc_csv)
         elif ins == "2":
             print("Query merged data")
-        elif ins == "3":
-            print("Do something else")
+            query()
         elif ins == "q":
             exit()
         else:
             print("Try again")
 
-# sudo pip install pandas
-# SystemError: Cannot compile 'Python.h'. Perhaps you need to install python-dev|python-devel.
 
 
-# script should support subsequent querying of the database to describe the average acceleration associated with each behaviour type
 
 
 
